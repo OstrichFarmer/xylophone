0e327f18f6b04cc89380efd5dfcc777a7c75c839
“Juelputa <joel.stephen56.sj@gmail.com>
2022-01-08 05:15:02 +0100

    playing a single not sound from the asset

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() {
   runApp(XylophoneApp());
 }
-
 class XylophoneApp extends StatelessWidget {
-  const XylophoneApp({Key? key}) : super(key: key);
+  // const XylophoneApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: TextButton(
+              onPressed: () {
+                final player = AudioCache();
+                player.play('assets_note1.wav');
+              },
+              child: Text('Play'),
+            ),
+          ),
         ),
       ),
     );
